797b6d0c66db4ad8ae71111e4e9b7c277c160273
Saba Schock <sababorjsaba@gmail.com>
2018-12-20 17:29:56 -0800

    adding front end

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,22 @@
 $(document).ready( function(){
-    $.getJSON('/api/users', function(data){
-        if(data){
-            for(var i=0;i<data.length;i++){
-                var tr = $("<tr>");
-                tr.append(`<td>${data[i].id}</td><td>${data[i].email}</td><td>${data[i].password}</td>`);
-                $("tbody").append(tr);
+    function getAllUserData(){
+        $.getJSON('/api/users', function(data){
+            if(data){
+                $("tbody").empty()
+                for(var i=0;i<data.length;i++){
+                    var tr = $("<tr>");
+                    tr.append(`<td>${data[i].id}</td><td>${data[i].email}</td><td>${data[i].password}</td>`);
+                    $("tbody").append(tr);
+                }
             }
-        }
-    })
+        })
+    }
+
+    getAllUserData();
+
+    // $("#user-form").on('submit', function(){
+    //     var id = 
+    //     var password = 
+    // })
+   
 })
